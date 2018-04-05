623cdbc7c541f5557785eb8b138b81f614b1658a
Niels Segers <segers.n@hotmail.com>
2018-04-05 18:32:48 +0200

    add filesearch to util

--- src/util/util.go
+++ src/util/util.go
@@ -2,8 +2,12 @@ package util
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
+	"os/user"
+	"path/filepath"
 	"reflect"
+	"sync"
 
 	"github.com/AlecAivazis/survey"
 )
@@ -42,4 +46,56 @@ func AskQuestion(question string, options []string) string {
 		os.Exit(0)
 	}
 	return answers.Selection
+}
+
+// Search : search object needed for SearchForFile
+type Search struct {
+	path string
+	name string
+}
+
+func scan(wg *sync.WaitGroup, folder string, depth int, results *[]Search, name string) {
+	defer wg.Done()
+
+	files, _ := ioutil.ReadDir(folder)
+	var directories []string
+
+	for _, file := range files {
+		path := folder + "/" + file.Name()
+
+		if file.IsDir() {
+			directories = append(directories, path)
+			continue
+		}
+
+		if !file.IsDir() && file.Name() == name {
+			*results = append(*results, Search{
+				path: filepath.Dir(path),
+				name: filepath.Base(filepath.Dir(path)),
+			})
+			return
+		}
+	}
+
+	if depth > 1 {
+		for _, folder := range directories {
+			wg.Add(1)
+			go scan(wg, folder, depth-1, results, name)
+		}
+	}
+
+	return
+}
+
+// SearchForFile : search home directory for a file and return all the results in an object containing path and name
+func SearchForFile(name string) []Search {
+	usr, _ := user.Current()
+	searches := []Search{}
+	var wg = new(sync.WaitGroup)
+
+	wg.Add(1)
+	go scan(wg, usr.HomeDir, 5, &searches, name)
+	wg.Wait()
+
+	return searches
 }
