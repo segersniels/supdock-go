bb20e8bb941873b79becdff1f057716abe214e1c
segersniels <segers.n@hotmail.com>
2018-06-20 23:50:58 +0200

    bump version

--- main.go
+++ main.go
@@ -84,7 +84,7 @@ func main() {
 			app := cli.NewApp()
 			app.Name = "supdock"
 			app.Usage = "What's Up Dock(er)?"
-			app.Version = "0.1.4-rc.1"
+			app.Version = "0.1.4-rc.2"
 			app.Commands = []cli.Command{
 				{
 					Name:  "logs",
